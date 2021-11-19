cae29119070ae9dc7bd5f3fc6494287aaf9d16c7
vicsanesp <48023240+vicsanesp@users.noreply.github.com>
2021-11-19 17:10:58 +0100

    Update Ejercicio5.java

--- PI3_vicsanesp/src/ejercicios/Ejercicio5.java
+++ PI3_vicsanesp/src/ejercicios/Ejercicio5.java
@@ -1,5 +1,6 @@
 package ejercicios;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -48,6 +49,39 @@ public class Ejercicio5 {
 			ej5Rec(arbolaso.getLeft(), res);
 			ej5Rec(arbolaso.getRight(), res);
 		
+		}
+		return res;
+	}
+	
+	public static Map<Paridad, List<Integer>> ej5It(BinaryTree<Integer> arbolaso){
+		Map<Paridad, List<Integer>> res = Map2.empty();
+		Iterator<BinaryTree<Integer>> it = arbolaso.iterator();
+		while(it.hasNext()) {
+			BinaryTree<Integer> aux = it.next();
+			if(aux.isBinary()) {
+				if(aux.getLeft().getLabel() < aux.getLabel() && aux.getLabel() < aux.getRight().getLabel()) {
+					if(aux.getLabel() % 2 == 0) {
+						if(res.containsKey(Paridad.Par)) {
+							res.get(Paridad.Par).add(aux.getLabel());
+						}
+						else {
+							List<Integer> par = List2.empty();
+							par.add(aux.getLabel());
+							res.put(Paridad.Par, par);
+						}
+					}
+					else {
+						if(res.containsKey(Paridad.Impar)) {
+							res.get(Paridad.Impar).add(aux.getLabel());
+						}
+						else {
+							List<Integer> impar = List2.empty();
+							impar.add(aux.getLabel());
+							res.put(Paridad.Impar, impar);
+						}
+					}
+				}
+			}
 		}
 		return res;
 	}
